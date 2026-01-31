a991ef7bc780eb3a53998c0462c3be94f316ac0a
BigBoa19 <choinoah2005@gmail.com>
2026-01-31 16:21:36 -0500

    keep_warm route

--- Front/TourGuide/app/camera.tsx
+++ Front/TourGuide/app/camera.tsx
@@ -237,7 +237,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
     zIndex: 1,
   },
   cameraOverlay: {
